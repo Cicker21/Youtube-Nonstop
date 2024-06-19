7a0b293690a90a508a3e36e49539f2a0f34e7209
Cicker <60420691+Cicker21@users.noreply.github.com>
2024-06-19 17:02:12 +0100

    Delete Nonstop directory

--- Nonstop/background.js
+++
@@ -1,8 +0,0 @@
-// Escucha eventos cuando se completa la carga de una pestaña
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url.includes("youtube.com")) {
-    browser.tabs.executeScript(tabId, { file: "content.js" })
-      .then(() => console.log("Content script injected into YouTube"))
-      .catch(error => console.error("Error Nonstop:", error));
-  }
-});
